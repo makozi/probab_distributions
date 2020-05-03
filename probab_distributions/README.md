71d75412a15e8c48fd25daa5912a1140864fb436
makozi <marizumakozi97@gmail.com>
2020-05-03 19:37:49 +0100

    Gaussian and Binomial distribution

---
+++ probab_distributions/README.md
@@ -0,0 +1,9 @@
+# prob-distribution
+
+### Gaussian distribution and Binomial distribution
+
+# Files
+
+Explanation of files in Package
+
+# Installation
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='probab_distributions',
+      version='1.0',
+      description='Gaussian and Binomial distribution',
+      packages=['probab_distributions'],
+      author='Marizu-Ibewiro Makozi',
+      author_email='marizumakozi97@gmail.com',
+      zip_safe=False)
